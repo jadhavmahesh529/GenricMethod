3df53bcd3827eb07e304545cb77d1362a0ed02fc
“Enter <jadhavmahesh529@gmail.com>
2023-05-30 17:09:35 +0530

    Add: Given 3 Floats find the maximum code is added

--- Program.cs
+++ Program.cs
@@ -8,8 +8,11 @@ namespace GenricMethod
         {
             TestClass testClass = new TestClass();
             int result = testClass.IntCompare(3, 4, 5);
-          
-            Console.WriteLine( result );
+
+            Console.WriteLine( result);
+            Testclass1 testClass1 = new Testclass1();
+            float result1 = testClass1.FloatCompare(1.1f, 2.2f, 3.3f);
+            Console.WriteLine( result1 );
           
         }
     }
