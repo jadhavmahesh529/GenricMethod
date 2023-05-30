b88605d0f3ba0b546e1c0dd7280348ce51c711d2
“Enter <jadhavmahesh529@gmail.com>
2023-05-30 15:10:47 +0530

    Add: compare three integers and find maximum number

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace GenricMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TestClass testClass = new TestClass();
+            int result = testClass.IntCompare(1, 2, 3);
+            Console.WriteLine( result );
+        }
+    }
+}
